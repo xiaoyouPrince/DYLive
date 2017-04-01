987209fd6f3cff6a18c836473cb8912c76c850e3
xiaoyouPrince <xiaoyouPrince@163.com>
2017-04-01 23:27:53 +0800

    使用构造方法设置NavBar
    继续修改UIBarButtonItem的扩展，之前用类方法来创建，现在使用自定义的构造方法创建item。

--- DYLive/DYLive/Classes/Home/Controller/HomeViewController.swift
+++ DYLive/DYLive/Classes/Home/Controller/HomeViewController.swift
@@ -39,19 +39,13 @@ extension HomeViewController{
         let size = CGSize(width: 40, height: 80)
         
         // logo
-        let logoItem = UIButton()
-        logoItem.setImage( UIImage(named:"logo"), for:.normal)
-        logoItem.sizeToFit()
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: logoItem)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "logo")
         
-        // 右边items
-        let historyItem = UIBarButtonItem.creatItem(imageName: "image_my_history", highImageName: "Image_my_history_click", size: size)
+        // 右边items--通过构造方法来创建对应的item
+        let historyItem = UIBarButtonItem(imageName: "image_my_history", highImageName: "Image_my_history_click", size: size)
+        let searchItem = UIBarButtonItem(imageName: "btn_search", highImageName: "btn_search_clicked", size: size)
+        let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size)
         
-        let searchItem = UIBarButtonItem.creatItem(imageName: "btn_search", highImageName: "btn_search_clicked", size: size)
-        
-        let qrcodeItem = UIBarButtonItem.creatItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size)
-        
-
         navigationItem.rightBarButtonItems = [historyItem,searchItem,qrcodeItem]
     }
 }
--- DYLive/DYLive/Classes/Tools/Extensions/UIBarButtonItem-Extension.swift
+++ DYLive/DYLive/Classes/Tools/Extensions/UIBarButtonItem-Extension.swift
@@ -11,21 +11,50 @@ import UIKit
 // 创建swift源文件，生成UIBarButtonItem的类扩展
 extension UIBarButtonItem{
     
-    // 创建类方法，生成Item，传参数和返回值
-    class func creatItem(imageName:String, highImageName:String, size:CGSize) -> UIBarButtonItem{
+//    // 创建类方法，生成Item，传参数和返回值
+//    class func creatItem(imageName:String, highImageName:String, size:CGSize) -> UIBarButtonItem{
+//        
+//        // 1.生成自定义btn
+//        let btn = UIButton()
+//        
+//        // 2.设置btn的nomal图片和highlight图片
+//        btn.setImage(UIImage(named: imageName), for: .normal)
+//        btn.setImage(UIImage(named: highImageName), for: .highlighted)
+//        
+//        // 3.设置btn的位置
+//        btn.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+//        
+//        // 4.返回根据btn生成的自定义barbuttonitem
+//        return UIBarButtonItem(customView: btn)
+//    }
+    
+    
+    // 遵守swift编码习惯，使用构造函数
+    // 便利构造函数：1.必须使用convenience开头，2.在构造函数中，必须明确的使用一个设计的构造函数（通过self调用）
+    // 方法的参数中，如果类型后面的 = "" ; 指的是可以为空
+    convenience init(imageName:String, highImageName:String = "", size:CGSize = CGSize(width: 0, height: 0)) {
+        
         
         // 1.生成自定义btn
         let btn = UIButton()
-        
+
         // 2.设置btn的nomal图片和highlight图片
         btn.setImage(UIImage(named: imageName), for: .normal)
-        btn.setImage(UIImage(named: highImageName), for: .highlighted)
+        
+        if highImageName != ""{
+            btn.setImage(UIImage(named: highImageName), for: .highlighted)
+        }
         
         // 3.设置btn的位置
-        btn.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+        if size == CGSize(width: 0, height: 0){
+            btn.sizeToFit()
+        }else
+        {
+            btn.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+        }
         
         // 4.返回根据btn生成的自定义barbuttonitem
-        return UIBarButtonItem(customView: btn)
+        self.init(customView: btn)
     }
     
 }
