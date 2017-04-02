155e35a191ca7b891c753370938cdc9ef3a8e410
xiaoyouPrince <xiaoyouPrince@163.com>
2017-04-02 23:43:45 +0800

    分析主页的页面逻辑
    主页UI逻辑是上面标题栏，下面对应的页面转换。 这个需要封装上下两个部分的view并对之进行业务处理

---
+++ DYLive/DYLive/Classes/Main/View/PageTitleView.swift
@@ -0,0 +1,25 @@
+//
+//  PageTitleView.swift
+//  DYLive
+//
+//  Created by 渠晓友 on 2017/4/1.
+//  Copyright © 2017年 xiaoyouPrince. All rights reserved.
+//
+
+import UIKit
+
+class PageTitleView: UIView {
+    
+    
+    // MARK:-首页的页面分析
+    // 1. 封装PageTitileView --> view：scrollview：Label+手势 & lineView
+    // 2. 封装PageContentView --> uicollectionView->横向滚动的cell
+    // 3. 处理PageTitleView和PageContentView的逻辑
+
+    // MARK:-自定制PageTitleView的构造方法
+    // 创建对应的PageTitleView
+
+
+    
+
+}
