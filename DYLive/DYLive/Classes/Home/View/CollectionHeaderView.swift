b9487d377e419e7374d6c71960f9e10e1f8da2b1
xiaoyouPrince <xiaoyouPrince@163.com>
2017-04-08 23:57:58 +0800

    自定义首页的collectionView的组头视图
    通过Xib来自定义首页的collectionView的组头视图

---
+++ DYLive/DYLive/Classes/Home/View/CollectionHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionHeaderView.swift
+//  DYLive
+//
+//  Created by 渠晓友 on 2017/4/8.
+//  Copyright © 2017年 xiaoyouPrince. All rights reserved.
+//
+
+import UIKit
+
+class CollectionHeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
