ec4cc50abf932c06e407e51c1f1df00935bf7de8
xiaoyouPrince <xiaoyouPrince@163.com>
2017-05-15 00:13:30 +0800

    实现全屏Pop手势
    实现全屏pop手势思路：
    通过得到NavigationBar系统Pop手势。systemGes -> gesView -> target/action.
    进行自定义一个PagGes手势，添加到原来gesView中，并添加对应target/action。
    
    其中可以用runtime来获取对应的方法等！

--- DYLive/DYLive/Classes/Main/Controller/CustomNavigationController.swift
+++ DYLive/DYLive/Classes/Main/Controller/CustomNavigationController.swift
@@ -13,6 +13,38 @@ class CustomNavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 1.拿到系统的返回手势
+        let systemGes = self.interactivePopGestureRecognizer!
+        
+        // 2.获取手势当前的view
+        guard let gesView = systemGes.view else {   return      }
+        
+        // 3.获取对应的target / action
+        var count : UInt32 = 0
+        let iVars = class_copyIvarList(UIGestureRecognizer.self, &count)!
+        
+        for i in 0..<count {
+            let ivar = iVars[Int(i)]
+            let name = ivar_getName(ivar)
+            print(String(cString : name!))
+        }
+        
+        guard let targets = systemGes.value(forKey: "_targets") as? [NSObject] else {  return }
+        guard let targetObj = targets.first else {return }
+        print(targetObj)
+        
+        // target
+        let target = targetObj.value(forKey: "target")
+        // action
+//        let action = targetObj.value(forKey: "action") as? Selector
+        let action = Selector(("handleNavigationTransition:")) //想方法获取到action
+        
+        
+        let panGes = UIPanGestureRecognizer()
+        gesView.addGestureRecognizer(panGes)
+        panGes.addTarget(target!, action: action)
+ 
     }
     
 
